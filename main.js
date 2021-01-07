8622cb22eb72187928919cbccb8ac74f6cb4e200
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2021-01-07 19:57:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -7,7 +12,11 @@ function setup(){
     poseNet.on('pose',gotPoses);
 }
 function draw(){
+    document.getElementById("square_side").innerHTML="Width And Height of square will be="+difference+"px"
     background('#32CD32');
+    fill('#228B22');
+    stroke('#228B22');
+    square(noseX,noseY,difference);
 }
 function modelLoaded(){
     console.log('poseNet is initiallized');
@@ -16,5 +25,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
        console.log(results);
+       noseX=results[0].pose.nose.x;
+       noseY=results[0].pose.nose.y;
+       console.log("Nose X="+noseX+"Nose Y="+noseY);
+       rightWristX=results[0].pose.rightWrist.x;
+       leftWristX=results[0].pose.leftWrist.x;
+       difference=floor(leftWristX - rightWristX);
+       console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX);
     }
 }
